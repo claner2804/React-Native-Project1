2ce75924f61559c582bd0fada8538b276ec37ad8
Claudia Steiner <claner2804@gmail.com>
2024-12-13 22:46:08 +0100

    .

--- App.js
+++ App.js
@@ -4,8 +4,20 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! :-)</Text>
+
+      <Text style ={styles.header}>Budget Planer </Text>
+      <Text style = {styles.bodyText}>dein Haushaltsbuch</Text>
+
       <StatusBar style="auto" />
+
+      <View style={styles.sectionContainer}>
+        <Text style={styles.sectionDescription}>
+          Budgetieren, Planen, Sparen, Investieren
+        </Text>
+      </View>
+
+
+
     </View>
   );
 }
@@ -13,8 +25,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    flexDirection: 'column',
+    backgroundColor: '#E5CCFF',
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 200,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  header: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    padding: 20,
+    color: '#4B0082',
+    backgroundColor: '#fff',
+  },
+  bodyText: {
+    fontSize: 20,
+    textAlign: 'center',
+    color: '#4B0082',
+    marginTop: 20,
   },
 });
